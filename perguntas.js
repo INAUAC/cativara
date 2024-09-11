2aabfa3625e9aab7265d9b63ee33d09ed0962106
INAUAC <160048476+INAUAC@users.noreply.github.com>
2024-09-11 14:47:23 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
+    'animes',
+    'O que é animes?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
+    'animes',
+    'quais sao os animes mais conhesidos?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'animes',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'animes',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
