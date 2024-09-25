ff6a7419f1b9663e67bbc576b501a20e484b2047
INAUAC <160048476+INAUAC@users.noreply.github.com>
2024-09-25 14:09:22 -0300

    qqq

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,60 @@
 criaCartao(
     'animes',
     'O que é animes?',
-    'O Python é uma linguagem de programação'
+    'No Brasil serve para definir as animações oriundas do Japão, entretanto para os japoneses caracteriza todo e qualquer tipo de animação independente de sua nacionalidade.'
 )
 
 criaCartao(
     'animes',
     'quais sao os animes mais conhesidos?',
-    'A capital da França é Paris'
+    'Dragon Bool, Naruto, Demon Sleier...'
 )
 
 criaCartao(
     'animes',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'o q assistir primeiro?',
+    'Fullmetal Alchemist Brotherhood.'
 )
 
 criaCartao(
     'animes',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'qual o meu preferido?',
+    'Tokio Revengers'
+)
+
+
+criaCartao(
+    'animes',
+    'resumo de Tokio Revengers?',
+    'Tokyo Revengers gira em torno de Takemichi Hanagaki, um jovem de 26 anos mal sucedido na vida, profissionalmente e em relações amorosas. Vive com problemas financeiros e ainda descobre que sua ex-namorada foi assassinada por uma gangue de Tóquio.'
+)
+
+criaCartao(
+    'animes',
+    'quem é Mikei?',
+    'Mikey, ou melhor, Sano Manjiro é um dos principais personagens de Tokyo Revengers por ser o líder da Tokyo Manji, a principal gangue da obra. Ele se tornou muito conhecido por suas habilidades de combate, principalmente seu chute potente, mas ele é reconhecido apenas por seu apelido, Mikey'
+)
+
+criaCartao(
+    'animes',
+    'quem é Draken?',
+    'Draken. Embora Mikey seja o grande líder da gangue, é Draken, cujo nome verdadeiro é Ken Ryuguji, que está na posição de vice-presidente do grupo, quem administra alguns dos seus principais negócios.'
+)
+
+criaCartao(
+    'animes',
+    'quem é o vilao?',
+    'kisaki Teta'
+)
+
+criaCartao(
+    'animes',
+    'quem é meu pessonagem prferido?',
+    'Kazutora Hanemia'
+)
+
+criaCartao(
+    'animes',
+    'quem é kazutora?',
+    'Kazutora é um membro da notória gangue conhecida como Tokyo Manji Gang. Ele também é um dos membros fundadores do grupo e é conhecido por ser um amigo próximo do líder do grupo, Mikey.'
 )
